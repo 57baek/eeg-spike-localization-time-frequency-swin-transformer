75a5709f3e6cc781e9917b0130dad099211593ca
HyangMok Baek <121350881+57baek@users.noreply.github.com>
2025-05-19 14:58:18 -0400

    Delete src directory

--- src/preprocessing/background_remover.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-
-def subtract_background(cwt_onset, cwt_baseline):
-    for i in range(cwt_onset.shape[0]):
-        baseline_mean = np.mean(cwt_baseline[i], axis=1).reshape(-1, 1)
-        cwt_onset[i] -= baseline_mean
-    return cwt_onset
--- src/utils/windowing.py
+++
@@ -1,3 +0,0 @@
-def extract_window(eeg, index, window_size, rate):
-    half_window = rate * window_size
-    return eeg[:, index - half_window : index + half_window]
