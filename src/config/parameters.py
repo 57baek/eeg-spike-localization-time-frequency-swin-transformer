17aa15531630bb62190056fe1eba48ea6563be15
HyangMok Baek <57baek@gmail.com>
2025-05-28 13:24:26 -0400

    3rd commit

--- src/config/parameters.py
+++ src/config/parameters.py
@@ -5,7 +5,7 @@ pre_sec = 30
 post_sec = 50
 min_segment_len_sec = 60
 
-eeg_ds_factor = 3 # 2
+eeg_ds_factor = 2
 
 # --------------------
 # time_frequency_analysis.2_time_frequency_transform
@@ -15,4 +15,4 @@ fmin = 1
 fmax = 150
 num_freq = 150
 
-tf_ds_factor = 33 # 50
+tf_ds_factor = 50
